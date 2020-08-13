30139321ad16a352b02a9a619864cfae50ad16a5
Andrew McConachie <andrew@depht.com>
2020-08-13 05:51:47 -0400

    An even simpler version for MX

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,8 @@ All rights reserved.
 //////////////
 // INCLUDES //
 //////////////
-#[macro_use] extern crate log;
 use structopt::StructOpt;
-//use std::{iter, time, thread};
-//use std::time::{Duration, SystemTime};
-use std::str::FromStr;
-//use std::net::Ipv4Addr;
-//use trust_dns_proto::DnsStreamHandle;
-use trust_dns_client::client::{Client, SyncClient};
-use trust_dns_client::udp::UdpClientConnection;
-use trust_dns_client::op::DnsResponse;
-use trust_dns_client::rr::{DNSClass, Name, RData, Record, RecordType};
+use trust_dns_resolver::Resolver;
 
 /////////////
 // STRUCTS //
@@ -37,42 +28,21 @@ struct Opt {
 // BEGIN PROGRAM EXECUTION //
 /////////////////////////////
 fn main() {
-    env_logger::builder().default_format_timestamp(false).init();
-    debug!("Start");
-
     let cli_opts: Opt = Opt::from_args();
 
-    ctrlc::set_handler(move || {
-        std::process::exit(0);
-    }).expect("Error setting Ctrl-C handler");
-
-    let dn: String = "_443._tcp.".to_owned() + &cli_opts.domain.to_lowercase() + ".";
+    let dn: String = "".to_owned() + &cli_opts.domain.to_lowercase() + ".";
     println!("{} {}", "querying", dn);
 
-    let address = "8.8.8.8:53".parse().unwrap();
-    let conn = UdpClientConnection::new(address).unwrap();
-    let client = SyncClient::new(conn);
-
-    let qstr = Name::from_str(&dn).unwrap();
-    let response: DnsResponse = client.query(&qstr, DNSClass::IN, RecordType::TLSA).unwrap();
-    let answers: &[Record] = response.answers();
-
-    if answers.len() > 0 {
-        for ans in answers.iter() {
-            if let RData::TLSA(rr) = ans.rdata() {
-                println!("{} {:?}", "\ncert_usage:", rr.cert_usage());
-                println!("{} {:?}", "selector:", rr.selector());
-                println!("{} {:?}", "matching:", rr.matching());
-                println!("{} {:?}", "hash:", rr.cert_data());
-            } else {
-                assert!(false, "unexpected result")
+    let resolver = Resolver::from_system_conf().unwrap();
+    let response = resolver.mx_lookup(&dn);
+    match response {
+        Ok(rrs) => {
+            for rdata in rrs.iter() {
+                println!("{:?}", rdata);
             }
         }
-    }else{
-        println!("{}", "No records found");
+        _ => println!("{}", "No records found"),
     }
-
-    debug!("Finish");
 }
 
 
